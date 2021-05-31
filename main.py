3f118636d8727572f5abc8e64c95255ceb210e83
Ziggi <SacharB@yandex.ru>
2021-06-01 00:05:51 +0400

    test vmute s/m/h/d

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    log_channel = discord.utils.get(message.member.guild.text_channels, name="log-zbh")  # что-то не так, смотри логи в
+    log_channel = discord.utils.get(message.author.guild.text_channels, name="log-zbh")  # что-то не так, смотри логи в
     #                                                                                      heroku
     if message.author == bot.user:
         content = message.content.split()
